d2229587f74f17c5dd273ec2b07daca562b19257
hay12396 <hay12396@gmail.com>
2017-07-29 10:19:29 +0300

    'isMenuOpen' method is added to the menuManager.

---
+++ HumanoidCharacter/AliveEnvironmentObjects/AgentConstants.js
@@ -0,0 +1,218 @@
+var AgentConstants = (function () {
+    function AgentConstants() {
+    }
+    Object.defineProperty(AgentConstants, "ON_PICK", {
+        get: function () { return "ON_PICK"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "ON_RELEASE", {
+        get: function () { return "ON_RELEASE"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "ON_MOVE", {
+        get: function () { return "ON_MOVE"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "CHARACTER_ACTIVATION", {
+        get: function () { return "CHARACTER_ACTIVATION"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "CHARACTER_DEACTIVATION", {
+        get: function () { return "CHARACTER_DEACTIVATION"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "ON_MOVE_RIGHT", {
+        get: function () { return "ON_MOVE_RIGHT"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "ON_MOVE_LEFT", {
+        get: function () { return "ON_MOVE_LEFT"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "ON_MOVE_UP", {
+        get: function () { return "ON_MOVE_UP"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "ON_MOVE_DOWN", {
+        get: function () { return "ON_MOVE_DOWN"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "ON_FALLING_LEFT", {
+        get: function () { return "ON_FALLING_LEFT"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "ON_FALLING_RIGHT", {
+        get: function () { return "ON_FALLING_RIGHT"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "SMS_RECEIVED", {
+        get: function () { return "SMS_RECEIVED"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "INCOMING_CALL", {
+        get: function () { return "INCOMING_CALL"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "NEW_OUTGOING_CALL", {
+        get: function () { return "NEW_OUTGOING_CALL"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "MENU_ACTION_FEED_ME", {
+        get: function () { return "Feed me."; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "MENU_ACTION_CURE_ME", {
+        get: function () { return "Cure me."; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "MENU_ACTION_REVIVE_ME", {
+        get: function () { return "Revive me."; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "ORIENTATION_PORTRAIT", {
+        get: function () { return 1; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "ORIENTATION_LANDSCAPE", {
+        get: function () { return 2; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "FUNNY_CATEGORY", {
+        get: function () { return "FUNNY"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "SCARY_CATEGORY", {
+        get: function () { return "SCARY"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "GENERAL_CATEGORY", {
+        get: function () { return "GENERAL"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "MINI_GAMES_CATEGORY", {
+        get: function () { return "MINI GAMES"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "REAL_PEOPLE_CATEGORY", {
+        get: function () { return "REAL PEOPLE"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "ADULT_CATEGORY", {
+        get: function () { return "ADULT"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "CARTOON_CATEGORY", {
+        get: function () { return "CARTOON"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "USER_EVENT_CHARACTER_UP_VOTE", {
+        get: function () { return "USER_EVENT_CHARACTER_UP_VOTE"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "USER_EVENT_CHARACTER_DOWN_VOTE", {
+        get: function () { return "USER_EVENT_CHARACTER_DOWN_VOTE"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "USER_EVENT_CHARACTER_DOWNLOAD_STARTED", {
+        get: function () { return "USER_EVENT_CHARACTER_DOWNLOAD_STARTED"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "USER_EVENT_CHARACTER_DOWNLOAD_CANCELED", {
+        get: function () { return "USER_EVENT_CHARACTER_DOWNLOAD_CANCELED"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "USER_EVENT_CHARACTER_DOWNLOAD_FINISHED", {
+        get: function () { return "USER_EVENT_CHARACTER_DOWNLOAD_FINISHED"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "USER_EVENT_CHARACTER_DELETED", {
+        get: function () { return "USER_EVENT_CHARACTER_DELETED"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "USER_EVENT_USER_CHANGE_PICTURE", {
+        get: function () { return "USER_EVENT_USER_CHANGE_PICTURE"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "USER_EVENT_USER_COMMENT", {
+        get: function () { return "USER_EVENT_USER_COMMENT"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AgentConstants, "IConfigurationManager", {
+        get: function () { return "IConfigurationManager"; },
+        enumerable: true,
+        configurable: true
+    });
+    ;
+    Object.defineProperty(AgentConstants, "IActionManager", {
+        get: function () { return "IActionManager"; },
+        enumerable: true,
+        configurable: true
+    });
+    ;
+    Object.defineProperty(AgentConstants, "ICharacterManager", {
+        get: function () { return "ICharacterManager"; },
+        enumerable: true,
+        configurable: true
+    });
+    ;
+    Object.defineProperty(AgentConstants, "IDatabaseManager", {
+        get: function () { return "IDatabaseManager"; },
+        enumerable: true,
+        configurable: true
+    });
+    ;
+    Object.defineProperty(AgentConstants, "IResourceManager", {
+        get: function () { return "IResourceManager"; },
+        enumerable: true,
+        configurable: true
+    });
+    ;
+    Object.defineProperty(AgentConstants, "IMenuManager", {
+        get: function () { return "IMenuManager"; },
+        enumerable: true,
+        configurable: true
+    });
+    ;
+    Object.defineProperty(AgentConstants, "IAwarenessManager", {
+        get: function () { return "IAwarenessManager"; },
+        enumerable: true,
+        configurable: true
+    });
+    return AgentConstants;
+}());
+;
+//# sourceMappingURL=AgentConstants.js.map
---
+++ HumanoidCharacter/app.js
@@ -0,0 +1,189 @@
+var AliveClass = (function () {
+    function AliveClass() {
+        this.currentVoiceIndex = 0;
+        this.voiceIndexSet = false;
+    }
+    AliveClass.prototype.onTick = function (time) {
+        if (this.voices == null || this.voices.length == 0)
+            this.voices = this.textToSpeechManager.getVoices();
+        var index = this.databaseManager.getObject("Index");
+        if (index != null) {
+            this.currentVoiceIndex = parseInt(index);
+            this.changeVoice(true);
+        }
+    };
+    AliveClass.prototype.changeVoice = function (force) {
+        if (this.voiceIndexSet && !force)
+            return;
+        this.voiceIndexSet = true;
+        var name = this.getVoiceTextPresentation(this.voices[this.currentVoiceIndex]);
+        this.menuManager.setProperty("LangTextBox", "Text", name);
+        this.textToSpeechManager.setVoice(this.currentVoiceIndex);
+        this.databaseManager.saveObject("Index", this.currentVoiceIndex.toString());
+        this.databaseManager.saveObject("VoiceName", name);
+    };
+    AliveClass.prototype.getVoiceTextPresentation = function (v) {
+        var gender = v.getName().indexOf("female") != -1 ? "female" : "";
+        if (gender == "")
+            gender = v.getName().indexOf("male") != -1 ? "male" : "unknown gender";
+        return v.getISO3Language() + " " + gender + " " + this.currentVoiceIndex.toString() + "/" + (this.voices.length - 1).toString();
+    };
+    AliveClass.prototype.onBackgroundTick = function (time) {
+    };
+    AliveClass.prototype.onStart = function (handler, disabledPermissions) {
+        this.configurationManager = handler.getConfigurationManager();
+        this.textToSpeechManager = handler.getTextToSpeechManager();
+        this.databaseManager = handler.getDatabaseManager();
+        this.actionManager = handler.getActionManager();
+        this.menuManager = handler.getMenuManager();
+        if (!this.textToSpeechManager.isAvailable()) {
+            handler.getActionManager().showSystemMessage("No Text-To-Speech Engine available, closing character..");
+            handler.getActionManager().terminate();
+        }
+    };
+    AliveClass.prototype.onPhoneEventOccurred = function (eventName, jsonedData) {
+        if (this.textToSpeechManager.isSpeaking()) {
+            return;
+        }
+        switch (eventName) {
+            case "CHARACTER_ACTIVATION":
+                this.maybeSay("Hello, my name is Humanoid, and i'm here to serve you.", 100);
+                break;
+            case "POWER_CONNECTED":
+                this.maybeSay("I will be here if you will need be.", 100);
+                break;
+            case "POWER_DISCONNECTED":
+                this.maybeSay("Thank you for charging me.", 100);
+                break;
+            case "AIRPLANE_MODE_OFF":
+                this.maybeSay("We are back to normal.", 100);
+                break;
+            case "AIRPLANE_MODE_ON":
+                this.maybeSay("The airplane mode is on.", 100);
+                break;
+            case "CALL_ENDED":
+                this.maybeSay("I hope you had a good talk.", 100);
+                break;
+            case "CLOSE_SYSTEM_DIALOGS":
+                this.maybeSay("Allright.", 5);
+                break;
+            case "HEADSET_PLUG_OFF":
+                this.maybeSay("Speakers are on.", 100);
+                break;
+            case "HEADSET_PLUG_ON":
+                this.maybeSay("Entering stealth mode.", 100);
+                break;
+            case "INCOMING_CALL":
+                this.maybeSay("We have an incoming call.", 100);
+                break;
+            case "MOBILE_INTERNET_OFF":
+                this.maybeSay("Alert! we are now offline.", 100);
+                break;
+            case "MOBILE_INTERNET_ON":
+                this.maybeSay("We are now online.", 100);
+                break;
+            case "NEW_OUTGOING_CALL":
+                this.maybeSay("Starting a call operation.", 100);
+                break;
+            case "PACKAGE_ADDED":
+                this.maybeSay("Package installation completed.", 100);
+                break;
+            case "PACKAGE_REMOVED":
+                this.maybeSay("Package was uninstalled successfully", 100);
+                break;
+            case "SCREEN_OFF":
+                this.maybeSay("Good bye.", 100);
+                break;
+            case "SCREEN_ON":
+                this.maybeSay("Ready to serve.", 100);
+                break;
+            case "WIFI_OFF":
+                this.maybeSay("Alert! we are now offline.", 100);
+                break;
+            case "WIFI_ON":
+                this.maybeSay("We are now online.", 100);
+                break;
+        }
+    };
+    AliveClass.prototype.maybeSay = function (text, chance) {
+        var randomChance = Math.random() * 100;
+        if (chance >= randomChance) {
+            this.textToSpeechManager.say(text);
+        }
+    };
+    AliveClass.prototype.onRelease = function (currentX, currentY) {
+    };
+    AliveClass.prototype.onPick = function (currentX, currentY) {
+    };
+    AliveClass.prototype.onMenuItemSelected = function (viewName) {
+        if (this.voices == null || this.voices.length == 0)
+            this.voices = this.textToSpeechManager.getVoices();
+        switch (viewName) {
+            case "PrevButton":
+                if (this.currentVoiceIndex > 0)
+                    this.currentVoiceIndex--;
+                else
+                    this.currentVoiceIndex = this.voices.length - 1;
+                break;
+            case "NextButton":
+                if (this.currentVoiceIndex == this.voices.length - 1)
+                    this.currentVoiceIndex = 0;
+                else
+                    this.currentVoiceIndex++;
+                break;
+        }
+        this.changeVoice(true);
+    };
+    AliveClass.prototype.onConfigureMenuItems = function (menuBuilder) {
+        var PrevButton = new ButtonMenuItem();
+        PrevButton.Height = 1;
+        PrevButton.Width = 2;
+        PrevButton.InitialX = 0;
+        PrevButton.InitialY = 3;
+        PrevButton.BackgroundColor = "#000000";
+        PrevButton.Text = "Back";
+        PrevButton.TextColor = "#0591de";
+        PrevButton.Name = "PrevButton";
+        var NextButton = new ButtonMenuItem();
+        NextButton.Height = 1;
+        NextButton.Width = 2;
+        NextButton.InitialX = 2;
+        NextButton.InitialY = 3;
+        NextButton.BackgroundColor = "#000000";
+        NextButton.Text = "Next";
+        NextButton.TextColor = "#0591de";
+        NextButton.Name = "NextButton";
+        var text = this.databaseManager.getObject("VoiceName");
+        if (text == null)
+            text = "Current Language: English";
+        var TextBox = new TextBoxMenuItem();
+        TextBox.BackgroundColor = "#000000";
+        TextBox.Height = 3;
+        TextBox.InitialX = 0;
+        TextBox.InitialY = 0;
+        TextBox.Name = "LangTextBox";
+        TextBox.Text = text;
+        TextBox.TextColor = "#0591de";
+        TextBox.Width = menuBuilder.getMaxColumns();
+        menuBuilder.createButton(PrevButton);
+        menuBuilder.createButton(NextButton);
+        menuBuilder.createTextBox(TextBox);
+    };
+    AliveClass.prototype.onSpeechRecognitionResults = function (results) { };
+    AliveClass.prototype.onResponseReceived = function (response) {
+    };
+    AliveClass.prototype.onLocationReceived = function (location) {
+    };
+    AliveClass.prototype.onUserActivityStateReceived = function (state) {
+    };
+    AliveClass.prototype.onPlacesReceived = function (places) {
+    };
+    AliveClass.prototype.onHeadphoneStateReceived = function (state) {
+    };
+    AliveClass.prototype.onWeatherReceived = function (weather) {
+    };
+    AliveClass.prototype.onUserEventOccurred = function (eventName, jsonedData) {
+    };
+    return AliveClass;
+}());
+//# sourceMappingURL=app.js.map
